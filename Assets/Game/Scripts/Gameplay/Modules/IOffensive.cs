324c7998e77498bcb3b70c3f828fe64038f60633
Stan Graafmans <stan_live@hotmail.nl>
2017-06-09 15:15:36 +0200

    Create IOffensive module

---
+++ Assets/Game/Scripts/Gameplay/Modules/IOffensive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CRC
+{
+    public interface IOffensive
+    {
+        KingTower Owner { get; }
+
+        float Damage { get; }
+        float AttackDelay { get; }
+        float AttackDelayInitial { get; }
+
+        Collider DefenseArea { get; }
+    }
+
+    public abstract class Offensive : Damageable, IOffensive
+    {
+        [SerializeField]
+        private float m_Damage;
+        public float Damage { get { return m_Damage; } }
+
+        [SerializeField]
+        private float m_AttackDelay;
+        public float AttackDelay { get { return m_AttackDelay; } }
+
+        [SerializeField]
+        private float m_AttackDelayInitial;
+        public float AttackDelayInitial { get { return m_AttackDelayInitial; } }
+
+        [SerializeField]
+        private Collider m_DefenseArea;
+        public Collider DefenseArea { get { return m_DefenseArea; } }
+
+        protected virtual void Awake()
+        {
+            m_DefenseArea = GetComponent<Collider>();
+        }
+    }
+}
