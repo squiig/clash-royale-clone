127793d49fffbc405da9a25683c7ebe36c74438e
Stan Graafmans <stan_live@hotmail.nl>
2017-06-15 11:33:19 +0200

    Fix scriptable objects problem

--- Assets/Game/Scripts/Gameplay/Units/Unit.cs
+++ Assets/Game/Scripts/Gameplay/Units/Unit.cs
@@ -7,26 +7,6 @@ using UnityEngine.UI;
 
 namespace CRC
 {
-    [CreateAssetMenu(fileName = "New Unit Definition")]
-    public class UnitDefinition : ScriptableObject
-    {
-        [SerializeField]
-        private string m_UnitName;
-        public string Name { get { return m_UnitName; } }
-
-        [SerializeField]
-        private float m_MovementSpeed;
-        public float MovementSpeed { get { return m_MovementSpeed; } }
-
-        [SerializeField]
-        private float m_Damage;
-        public float Damage { get { return m_Damage; } }
-
-        [SerializeField]
-        private float m_DamageInterval;
-        public float DamageInterval { get { return m_DamageInterval; } }
-    }
-
     public class Unit : Damageable
     {
         public enum UnitState
